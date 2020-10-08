02e55bba1c392df2991978e8167477f644d6c0e5
Zach Holland <zack09holland@gmail.com>
2020-10-08 15:29:15 -0700

    Tried to up my design skills with some 'fancy' styling to the container and background

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import './JokeList.css'
 import axios from "axios";
+
 class JokeList extends Component {
 	constructor(props) {
 		super(props);
@@ -11,7 +13,7 @@ class JokeList extends Component {
 		numJokesToGet: 10,
 	};
 	async componentDidMount() {
-		// Load jokes from API and push into state
+		// Load jokes from API and set the state to hold them
 		let jokes = [];
 		while (jokes.length < this.props.numJokesToGet) {
 			let url = "https://icanhazdadjoke.com/";
@@ -24,8 +26,14 @@ class JokeList extends Component {
 	}
 	render() {
 		return (
-            <div>
-                <h1> Dad Jokes</h1>
+            <div className="JokeList">
+                <div className="JokeList-sidebar">
+                    <h1 className="JokeList-title">
+                        <span>Dad</span>Jokes
+                    </h1>
+                    <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt='emoji'/> 
+                </div>
+                
                 <div className='JokeList-jokes'>
                     {this.state.jokes.map(joke => (
                         <div>
