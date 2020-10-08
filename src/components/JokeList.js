ffc1fb36c4757b261a39b2ce742cffae296bd77e
Zach Holland <zack09holland@gmail.com>
2020-10-08 15:06:08 -0700

    Got jokes displayed to page

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 class JokeList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    static defaultProps = {
-        numJokesToGet : 10
-    }
-    async componentDidMount() {
-        let url = 'https://icanhazdadjoke.com/'
-        let res = await axios.get(url, {headers: {Accept: 'application/json'}})
-        console.log(res)
-    }
-    render() { 
-        return ( 
+	constructor(props) {
+		super(props);
+		this.state = {
+			jokes: [],
+		};
+	}
+	static defaultProps = {
+		numJokesToGet: 10,
+	};
+	async componentDidMount() {
+		// Load jokes from API and push into state
+		let jokes = [];
+		while (jokes.length < this.props.numJokesToGet) {
+			let url = "https://icanhazdadjoke.com/";
+			let res = await axios.get(url, {
+				headers: { Accept: "application/json" },
+			});
+			jokes.push(res.data.joke);
+		}
+		this.setState({ jokes: jokes });
+	}
+	render() {
+		return (
             <div>
-
+                <h1> Dad Jokes</h1>
+                <div className='JokeList-jokes'>
+                    {this.state.jokes.map(joke => (
+                        <div>
+                            {joke}
+                        </div>
+                    ))}
+                </div>
             </div>
-         );
-    }
+        ) 
+	}
 }
- 
+
 export default JokeList;
