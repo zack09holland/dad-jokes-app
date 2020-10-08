cf0c939adf1633fb02da20739f62b8d72311dab3
Zach Holland <zack09holland@gmail.com>
2020-10-08 16:59:53 -0700

    Got vote functionality working for each of the jokes passed in, will still need to configure local storage so votes are saved to the paticular joke overtime

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Joke.css";
 
 class Joke extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>
-                
-            </div>
-         );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+        let {upvote,downvote,jokeText,votes} = this.props
+		return (
+			<div className="Joke">
+				<div className="Joke-button">
+					<i onClick={upvote} className="fas fa-arrow-up"></i>
+					<span>{votes}</span>
+					<i onClick={downvote} className="fas fa-arrow-down"></i>
+				</div>
+
+				<div className="Joke-text">{jokeText}</div>
+			</div>
+		);
+	}
 }
- 
+
 export default Joke;
--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import './JokeList.css'
+import Joke from "./Joke";
+import "./JokeList.css";
 import axios from "axios";
+import uuid from "uuid/dist/v4";
 
 class JokeList extends Component {
 	constructor(props) {
@@ -9,9 +11,11 @@ class JokeList extends Component {
 			jokes: [],
 		};
 	}
+
 	static defaultProps = {
 		numJokesToGet: 10,
 	};
+
 	async componentDidMount() {
 		// Load jokes from API and set the state to hold them
 		let jokes = [];
@@ -20,29 +24,51 @@ class JokeList extends Component {
 			let res = await axios.get(url, {
 				headers: { Accept: "application/json" },
 			});
-			jokes.push(res.data.joke);
+			jokes.push({ id: uuid(), jokeText: res.data.joke, votes: 0 });
 		}
 		this.setState({ jokes: jokes });
 	}
+	// Function to handle whether a vote is upvoted or downvoted
+	//  - Map over the joke and find the joke ID passed in
+	//      - Make a new object with the votes and update the votes value
+	//  - Else just add the joked unchanged
+	handleVote(id, delta) {
+		this.setState((prevState) => {
+			return {
+				jokes: prevState.jokes.map((joke) =>
+					joke.id === id ? { ...joke, votes: joke.votes + delta } : joke
+				),
+			};
+		});
+	}
+
 	render() {
 		return (
-            <div className="JokeList">
-                <div className="JokeList-sidebar">
-                    <h1 className="JokeList-title">
-                        <span>Dad</span>Jokes
-                    </h1>
-                    <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt='emoji'/> 
-                </div>
-                
-                <div className='JokeList-jokes'>
-                    {this.state.jokes.map(joke => (
-                        <div>
-                            {joke}
-                        </div>
-                    ))}
-                </div>
-            </div>
-        ) 
+			<div className="JokeList">
+				<div className="JokeList-sidebar">
+					<h1 className="JokeList-title">
+						<span>Dad</span>Jokes
+					</h1>
+					<img
+						src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+						alt="emoji"
+					/>
+					<button className="JokeList-btn">New Jokes</button>
+				</div>
+
+				<div className="JokeList-jokes">
+					{this.state.jokes.map((j) => (
+						<Joke
+							key={j.id}
+							votes={j.votes}
+							jokeText={j.jokeText}
+							upvote={() => this.handleVote(j.id, 1)}
+							downvote={() => this.handleVote(j.id, -1)}
+						/>
+					))}
+				</div>
+			</div>
+		);
 	}
 }
 
