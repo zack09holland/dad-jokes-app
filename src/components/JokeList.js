3566637fe57d2af7a296aab5f459e346a169992d
Zach Holland <zack09holland@gmail.com>
2020-10-09 11:30:14 -0700

    Created functionality to prevent duplicate jokes from appearing

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -13,7 +13,10 @@ class JokeList extends Component {
 		super(props);
 		this.state = {
 			jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+			loading: false,
 		};
+		//Variable to hold the seen jokes so we dont get duplicates
+		this.seenJokes = new Set(this.state.jokes.map((joke) => joke.text));
 		this.fetchNewJokes = this.fetchNewJokes.bind(this);
 	}
 
@@ -24,29 +27,40 @@ class JokeList extends Component {
 		}
 	}
 	async getjokes() {
-		// Load jokes from API and set the state to hold them
-		let jokes = [];
-		while (jokes.length < this.props.numJokesToGet) {
-			let url = "https://icanhazdadjoke.com/";
-			let res = await axios.get(url, {
-				headers: { Accept: "application/json" },
-			});
-			jokes.push({ id: uuid(), jokeText: res.data.joke, votes: 0 });
-		}
-		//Set the state with the old jokes and the new jokes loaded in
-		this.setState(
-			(prevState) => {
-				return { jokes: [...prevState.jokes, ...jokes] };
-			}, //Save the new jokes loaded in to local storage
-			() =>
-				window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
-		);
-    }
-    
+		try {
+			// Load jokes from API and set the state to hold them
+			let jokes = [];
+			while (jokes.length < this.props.numJokesToGet) {
+				let url = "https://icanhazdadjoke.com/";
+				let res = await axios.get(url, {
+					headers: { Accept: "application/json" },
+				});
+				if (this.seenJokes.has(res.data.joke)) {
+					jokes.push({ id: uuid(), jokeText: res.data.joke, votes: 0 });
+				} else {
+					console.log("Found a duplicate");
+				}
+			}
+			//Set the state with the old jokes and the new jokes loaded in
+			this.setState(
+				(prevState) => {
+					return { loading: false, jokes: [...prevState.jokes, ...jokes] };
+				}, //Save the new jokes loaded in to local storage
+				() =>
+					window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+			);
+		} catch (error) {
+			alert(error);
+            this.setState({loading : false})
+        }
+	}
+
 	// Fetches 10 new jokes from the API
 	fetchNewJokes() {
-		this.getjokes();
+		//Set the state so loading is true for the loading icon and then immediately call getJokes function
+		this.setState({ loading: true }, this.getjokes);
 	}
+
 	// Function to handle whether a vote is upvoted or downvoted
 	//  - Map over the joke and find the joke ID passed in
 	//      - Make a new object with the votes and update the votes value
@@ -67,6 +81,14 @@ class JokeList extends Component {
 	}
 
 	render() {
+		if (this.state.loading) {
+			return (
+				<div className="JokeList-spinner">
+					<i className="far fa-8x fa-laugh fa-spin"></i>
+					<h1 className="JokeList-title">Loading...</h1>
+				</div>
+			);
+		}
 		return (
 			<div className="JokeList">
 				<div className="JokeList-sidebar">
